232cedb8417192e233b5ee45078771a49f59eec9
Ederson Silva <edersonsilva@MacBook-Air-de-Ederson.local>
2023-08-28 17:53:36 -0300

    primeiro commit

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+
+$hostname="localhost";
+$username="root";
+$password="";
+$dbname="gesc";
+
+$conexao = mysqli_connect($hostname,$username, $password);
+$sql = mysqli_select_db($conexao, $dbname);
+
+if(!$conexao) {
+  die("Conexão falhou" .mysqli_connect_error() );
+}
+
+?>
