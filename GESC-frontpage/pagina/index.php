24946f152f0847ef52fc37a72efeed76e74f1fd5
lucasmarques2907 <lucasmarques2907@gmail.com>
2023-09-05 20:53:39 -0300

    adicionando página

--- GESC-frontpage/pagina/index.php
+++ GESC-frontpage/pagina/index.php
@@ -50,32 +50,9 @@ include_once 'header.php';
                     <div class="textCard">
                         <h3 class="card-title">Titulo</h3>
                     </div>
-                    <!-- MODAL -->
-                    <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+                    <a type="button" class="btn btn-primary" href="evento.php">
                         Mais detalhes
-                    </button>
-                    <!-- Modal -->
-                    <div class="modal fade" id="exampleModalCenter" data-bs-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-                                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    ...
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save changes</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- FIM MODAL -->
+                    </a>
                 </div>
             </div>
         </div>
