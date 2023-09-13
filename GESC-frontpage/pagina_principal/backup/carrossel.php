1243cb5bf0e25b2e7e057c852808cf9bba733aa2
lucasmarques2907 <lucasmarques2907@gmail.com>
2023-09-13 19:45:27 -0300

    deletando pasta adicional

--- GESC-frontpage/pagina_principal/backup/carrossel.php
+++
@@ -1,4 +0,0 @@
-<form action="evento.php" method="post">
-                            <input type="hidden" name="Titulo" value="<?= $aux["nome"] ?>" />
-                            <input type="image" src="<?= $aux["imagem"] ?>" class="d-block w-100" alt="..." />
-                        </form>
