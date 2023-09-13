1302ca1044654978270fb459207c803036a01118
lucasfelix93 <lucas_felix-silva@estudante.sc.senai.br>
2023-09-12 21:18:39 -0300

    Alteracoes páginas de avaliacao de evento

---
+++ GESC/GESC-frontpage/pagina/evento.php
@@ -0,0 +1,24 @@
+<?php
+include_once 'header.php';
+?>
+<!-- Página do evento que foi selecionado!-->
+
+<div>
+    <div> 
+        <img src="https://placehold.co/800x400" class="img-fluid" alt="...">
+        <section>
+            <p class="descricao">Nome do EVENTO.</p>
+            <figure class="quote">
+            <blockquote>Sophisticated foods <em>(wine, beer, coffee, chocolate)</em> are bittersweet. Addictive relationships are cooperative and competitive. Work becomes flow at the limits of ability. The flavor of life is on the <strong>edge</strong>.
+            </blockquote>
+            <button type="submit" class="participarButton" href="evento.php">Quero participar</button>
+            </figure>
+        </section>
+   </div>
+</div>
+
+    
+
+<?php
+include_once 'footer.php';
+?>
