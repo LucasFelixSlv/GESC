4e7e9341c0053e673be67ae04055ab645cc49497
lucasmarques2907 <lucasmarques2907@gmail.com>
2023-09-21 21:06:35 -0300

    Adicionando sessão de comentários nos eventos

--- GESC-frontpage/pagina_principal/evento.php
+++ GESC-frontpage/pagina_principal/evento.php
@@ -4,7 +4,7 @@ include_once 'header.php';
 if (isset($_GET['id'])) {
     include_once '../includes/dbh.inc.php';
     $linkEvento = $_GET['id'];
-    if(isset($_SESSION["usuariosId"])){
+    if (isset($_SESSION["usuariosId"])) {
         $usuariosId = $_SESSION["usuariosId"];
     }
     $linkEvento = mysqli_real_escape_string($conexao, $linkEvento);
@@ -14,7 +14,7 @@ if (isset($_GET['id'])) {
         $sql = mysqli_query($conexao, "SELECT * FROM eventos WHERE link = '$linkEvento'");
         $aux = mysqli_fetch_assoc($sql);
 
-        $eventosId = $aux["eventosId"];        
+        $eventosId = $aux["eventosId"];
 
         date_default_timezone_set('America/Sao_Paulo');
         $dataAtual = date('d/m/Y');
@@ -50,39 +50,70 @@ if (isset($_GET['id'])) {
             echo '<br>Você não pode solicitar participação!';
             if (isset($usuariosId)) {
                 $sqlAceito = mysqli_query($conexao, "SELECT * FROM solicitacao WHERE usuariosId = '$usuariosId' AND eventosId = '$eventosId' AND aprovado = 'SIM'");
-                if(mysqli_num_rows($sqlAceito) === 1){ //se a solicitação foi aceita, colocá-lo na tabela participacao_eventos e retirar da solicitacao
+                if (mysqli_num_rows($sqlAceito) === 1) { //se a solicitação foi aceita, colocá-lo na tabela participacao_eventos e retirar da solicitacao
                     //essa parte será colocada na parte de admin
                     //para aceitar a solicitação precisa ir no banco de dados e na coluna "aprovado" da tabela "solicitacao" e digitar SIM;
                     mysqli_query($conexao, "INSERT INTO participacao_eventos (usuariosId, eventosId) VALUES ('$usuariosId', '$eventosId')");
                     mysqli_query($conexao, "DELETE FROM solicitacao WHERE usuariosId = '$usuariosId' AND eventosId = '$eventosId'");
                 }
-                    //verifica se o usuário está participando e se ainda não avaliou o evento
-                    $sqlParticipacao = mysqli_query($conexao, "SELECT * FROM participacao_eventos WHERE eventosId = '$eventosId' AND usuariosId='$usuariosId'");
-                    $sqlComentarios = mysqli_query($conexao, "SELECT * FROM avaliacoes INNER JOIN participacao_eventos ON avaliacoes.participacaoId = participacao_eventos.participacaoId WHERE participacao_eventos.usuariosId = '$usuariosId' AND participacao_eventos.eventosId = '$eventosId'");
-                    if (mysqli_num_rows($sqlParticipacao) === 1 && mysqli_num_rows($sqlComentarios) === 0) {
-                        $participacaoId = mysqli_fetch_assoc($sqlParticipacao);
+                //verifica se o usuário está participando e se ainda não avaliou o evento
+                $sqlParticipacao = mysqli_query($conexao, "SELECT * FROM participacao_eventos WHERE eventosId = '$eventosId' AND usuariosId='$usuariosId'");
+                $sqlComentarios = mysqli_query($conexao, "SELECT * FROM avaliacoes INNER JOIN participacao_eventos ON avaliacoes.participacaoId = participacao_eventos.participacaoId WHERE participacao_eventos.usuariosId = '$usuariosId' AND participacao_eventos.eventosId = '$eventosId'");
+                if (mysqli_num_rows($sqlParticipacao) === 1 && mysqli_num_rows($sqlComentarios) === 0) {
+                    $participacaoId = mysqli_fetch_assoc($sqlParticipacao);
                     //form de avaliação
-                    ?>
-                        <form action="../includes/eventComment.inc.php" method="post">
-                            <input type="hidden" name="participacaoId" value="<?= $participacaoId["participacaoId"] ?>">
-                            <input type="hidden" name="linkEvento" value="<?= $linkEvento ?>">
-                            <input type="number" name="nota" min="1" max="5" step="0.5" value="0">
-                            <textarea name="comentario" rows="4" cols="50"></textarea>
-                            <button type="submit" name="enviar">Enviar</button>
-                        </form>
-    <?php
+                ?>
+                    <form action="../includes/eventComment.inc.php" method="post">
+                        <input type="hidden" name="participacaoId" value="<?= $participacaoId["participacaoId"] ?>">
+                        <input type="hidden" name="linkEvento" value="<?= $linkEvento ?>">
+                        <input type="number" name="nota" min="1" max="5" step="0.5" value="0">
+                        <textarea name="comentario" rows="4" cols="50"></textarea>
+                        <button type="submit" name="enviar">Enviar</button>
+                    </form>
+            <?php
                 }
             }
-        }
-        //mostrando todos os comentários do evento passado
-        $sqlMostraComentarios = mysqli_query($conexao, "SELECT usuarios.usuariosNome, avaliacoes.comentario, avaliacoes.nota FROM avaliacoes INNER JOIN participacao_eventos ON avaliacoes.participacaoId = participacao_eventos.participacaoId INNER JOIN usuarios ON participacao_eventos.usuariosId = usuarios.usuariosId WHERE eventosId = '$eventosId'");
-        while($comentarios = mysqli_fetch_assoc($sqlMostraComentarios)){
-            //html com os comentarios abaixo
-            ?>
-            <p>Usuário: <?=$comentarios["usuariosNome"]?></p>
-            <p>Nota: <?=$comentarios["nota"]?></p>
-            <p>Comentário: <?=$comentarios["comentario"]?></p>
-            <?php
+            //mostrando todos os comentários do evento passado
+            $sqlMostraComentarios = mysqli_query($conexao, "SELECT usuarios.usuariosNome, avaliacoes.comentario, avaliacoes.nota FROM avaliacoes INNER JOIN participacao_eventos ON avaliacoes.participacaoId = participacao_eventos.participacaoId INNER JOIN usuarios ON participacao_eventos.usuariosId = usuarios.usuariosId WHERE eventosId = '$eventosId'");
+            if (mysqli_num_rows($sqlMostraComentarios) > 0) {
+                ?>
+                <div class="container">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-8 col-lg-6">
+                        <div class="card shadow-0 border" style="background-color: #f0f2f5;">
+                            <div class="card-body p-4 containerComentarios">
+                                <?php
+    
+                                while ($comentarios = mysqli_fetch_assoc($sqlMostraComentarios)) {
+                                    //html com os comentarios abaixo
+                                ?>
+    
+                                    <div class="card mb-0 mt-2">
+                                        <div class="card-body">
+                                            <div class="d-flex justify-content-between mb-2">
+                                                <div class="d-flex flex-row align-items-center">
+                                                    <i class="fa-solid fa-user" style="color: #42c82c;"></i>
+                                                    <p class="small mb-0 ms-2" style="font-weight: bold;"><?= $comentarios["usuariosNome"] ?></p>
+                                                </div>
+                                                <div class="d-flex flex-row align-items-center">
+                                                    <p class="small text-muted mb-0" style="font-weight: bold;">Nota:&nbsp;</p>
+                                                    <p class="small text-muted mb-0" style="font-weight: bold;"><?= $comentarios["nota"] ?></p>
+                                                    <i class="far fa-solid fa-star mx-2 fa-xs" style="color: #42c82c;"></i>
+                                                </div>
+                                            </div>
+                                            <p class="comentario mb-0"><?= $comentarios["comentario"] ?></p>
+                                        </div>
+                                    </div>
+                                <?php
+                                }
+                                ?>
+    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    <?php
+            }
         }
     } else {
         echo "Nenhum evento registrado com este link.";
