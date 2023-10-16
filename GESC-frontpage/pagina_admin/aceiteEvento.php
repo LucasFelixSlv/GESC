30878006da90c63d370d3fe4044ee86e795f76cf
lucasmarques2907 <lucasmarques2907@gmail.com>
2023-10-16 20:01:44 -0300

    Eventos estão sendo salvos e editados com sucesso, usuários que não estão logados não conseguem acessar as páginas de admin

--- GESC-frontpage/pagina_admin/aceiteEvento.php
+++ GESC-frontpage/pagina_admin/aceiteEvento.php
@@ -1,39 +1,45 @@
 <?php
 
 include '../includes/dbh.inc.php';
+session_start();
+if (isset($_SESSION["usuariosId"])) {
 
-if (isset($_POST['aceitar'])) {
+    if (isset($_POST['aceitar'])) {
 
-    $nomeUsuario = $_POST['nomeUsuario'];
-    $email = $_POST['email'];
-    $telefone = $_POST['telefone'];
-    $aprovado = $_POST['aprovado'];
+        $nomeUsuario = $_POST['nomeUsuario'];
+        $email = $_POST['email'];
+        $telefone = $_POST['telefone'];
+        $aprovado = $_POST['aprovado'];
 
-    $sql = "INSERT INTO `aceiteEvento` (nomeUsuario, aprovado) VALUES ('$nomeUsuario', '$aprovado')";
-    $rs = mysqli_query($conexao, $sql);
-}
+        $sql = "INSERT INTO `aceiteEvento` (nomeUsuario, aprovado) VALUES ('$nomeUsuario', '$aprovado')";
+        $rs = mysqli_query($conexao, $sql);
+    }
 
 ?>
 
-<!DOCTYPE html>
-<html>
+    <!DOCTYPE html>
+    <html>
 
-<head>
-    <title>Formulário de Registro de Evento</title>
-</head>
+    <head>
+        <title>Formulário de Registro de Evento</title>
+    </head>
 
-<body>
-    <h1>Formulário de Registro de Evento</h1>
-    <p>Preencha o formulário abaixo para se registrar para o evento:</p>
+    <body>
+        <h1>Formulário de Registro de Evento</h1>
+        <p>Preencha o formulário abaixo para se registrar para o evento:</p>
 
-    <form action="aceiteEvento.php" method="POST">
-        <label for="nome">Nome:</label>
-        <input type="text" id="nomeUsuario" name="nomeUsuario" required><br><br>
+        <form action="aceiteEvento.php" method="POST">
+            <label for="nome">Nome:</label>
+            <input type="text" id="nomeUsuario" name="nomeUsuario" required><br><br>
 
-        <input type="hidden" id="aprovado" name="aprovado" value="Não"><br><br>
+            <input type="hidden" id="aprovado" name="aprovado" value="Não"><br><br>
 
-        <button type="submit" name="aceitar" class="btn btn btn-success">Salvar Evento</button>
-    </form>
-</body>
+            <button type="submit" name="aceitar" class="btn btn btn-success">Salvar Evento</button>
+        </form>
+    </body>
 
-</html>
+    </html>
+<?php
+} else {
+    header('location: ../pagina_principal/index.php');
+}
--- GESC-frontpage/pagina_admin/tt.php
+++ GESC-frontpage/pagina_admin/tt.php
@@ -1,39 +1,45 @@
 <?php
 
 include '../includes/dbh.inc.php';
+session_start();
+if (isset($_SESSION["usuariosId"])) {
 
-if (isset($_POST['adicionarComentario'])) {
+    if (isset($_POST['adicionarComentario'])) {
 
-    $participacaoId = $_POST['participacaoId'];
-    $comentario = $_POST['comentario'];
-}
+        $participacaoId = $_POST['participacaoId'];
+        $comentario = $_POST['comentario'];
+    }
 
-$sql = "INSERT INTO `avaliacoes` (participacaoId, comentario) VALUES ('$participacaoId', '$comentario')";
+    $sql = "INSERT INTO `avaliacoes` (participacaoId, comentario) VALUES ('$participacaoId', '$comentario')";
 
-$rs = mysqli_query($conexao, $sql);
+    $rs = mysqli_query($conexao, $sql);
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário de Comentário</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Formulário de Comentário</title>
+    </head>
 
-<body>
-    <h1>Deixe seu Comentário</h1>
-    <form action="tt.php" method="post">
-        <label for="nome">Nome:</label>
-        <input type="text" id="nomeUsuario" name="nomeUsuario" required><br><br>
+    <body>
+        <h1>Deixe seu Comentário</h1>
+        <form action="tt.php" method="post">
+            <label for="nome">Nome:</label>
+            <input type="text" id="nomeUsuario" name="nomeUsuario" required><br><br>
 
-        <label for="comentario">Comentário:</label>
-        <textarea id="comentario" name="comentario" rows="4" cols="50" required></textarea><br><br>
+            <label for="comentario">Comentário:</label>
+            <textarea id="comentario" name="comentario" rows="4" cols="50" required></textarea><br><br>
 
-        <input type="submit" name="adicionarComentario" value="Enviar Comentário">
-    </form>
-</body>
+            <input type="submit" name="adicionarComentario" value="Enviar Comentário">
+        </form>
+    </body>
 
-</html>
+    </html>
+<?php
+} else {
+    header('location: ../pagina_principal/index.php');
+}
